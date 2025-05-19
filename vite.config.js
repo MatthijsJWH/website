f1628f0ad6bdcfaffa575312fba027556a28d121
Matthijs Halvemaan <matthijs.halvemaan@gmail.com>
2025-05-20 00:38:51 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite"
+import { defineConfig } from "vite"
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		global: true,
-		environment: 'jsdom',
-		include: ['tests/**/*.test.js', 'src/lib/**/*.test.js'],
-		exclude: ['src/routes/**/*.{test,spec}.{js,ts,svelte}'],
-		coverage: {
-			reporter: ['text', 'json', 'html']
-		}
-	}
-});
+  plugins: [sveltekit()],
+  test: {
+    global: true,
+    environment: "jsdom",
+    include: ["tests/**/*.test.js", "src/lib/**/*.test.js"],
+    exclude: ["src/routes/**/*.{test,spec}.{js,ts,svelte}"],
+    coverage: {
+      reporter: ["text", "json", "html"],
+    },
+  },
+  handleHttpError: "warn",
+})
