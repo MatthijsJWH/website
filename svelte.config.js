6afddf3d5fd33522410e4a6b9ffcedafc84e8433
Matthijs Halvemaan <matthijs.halvemaan@gmail.com>
2025-05-20 00:44:13 +0200

    Update CI/CD workflow to deploy from build directory and adjust Svelte config fallback option

--- svelte.config.js
+++ svelte.config.js
@@ -9,13 +9,13 @@ const config = {
       // these options are set automatically — see below
       pages: "build",
       assets: "build",
-      fallback: "404.html",
+      fallback: undefined,
       precompress: false,
       strict: true,
     }),
-    paths: {
-      base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
-    },
+    // paths: {
+    //   base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
+    // },
     prerender: {
       handleHttpError: "warn",
     },
